e119e7b294a7a0beb8c1b45b0833fc0376ff8c05
Rui <marui1@ruijie.com.cn>
2024-07-19 16:58:39 +0800

    add log-viwer

--- src/components/LogView.vue
+++ src/components/LogView.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
+import { RecycleScroller } from 'vue-virtual-scroller'
 
-const log = ref([])
+const items = ref([
+  { id: 1, message: 'Log message 1' },
+  { id: 2, message: 'Log message 2' }
+])
+
+for (let i = 0; i < 100; i++) {
+  items.value.push({ id: i + 3, message: `aaaa: ${i}` })
+}
 </script>
 
 <template>
-    <div>这里是日志</div>
+    <RecycleScroller class="w-full h-full p-4 bg-black text-white" :items="items" :item-size="28" key-field="id">
+      <template #default="{ item, index }">
+        <div class="font-bold">
+            {{ index }}: {{ item.message }}
+        </div>
+      </template>
+    </RecycleScroller>
 </template>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,15 +1,10 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
 import LogView from '@/components/LogView.vue';
-
-const v = ref('package main')
 
 </script>
 
 <template>
-  <div class="w-full h-full p-4">
-    <!-- <ContainerView host-id="5d0243bd-4814-41de-848a-e7c80fc39290" /> -->
-<!--    <CodeEditor language="go" :read-only="false" v-model:value="v" />-->
+  <div class="w-full h-[300px]">
     <LogView />
   </div>
 </template>
