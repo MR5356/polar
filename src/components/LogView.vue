2d1e063d878fa9913b967b09cf40939c235c3569
Rui <marui1@ruijie.com.cn>
2024-07-24 14:12:19 +0800

    add logview[test]

--- src/components/LogView.vue
+++ src/components/LogView.vue
@@ -1,24 +1,62 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import { RecycleScroller } from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
-const items = ref([
-  { id: 1, message: 'Log message 1' },
-  { id: 2, message: 'Log message 2' }
-])
-
-for (let i = 0; i < 100; i++) {
-  items.value.push({ id: i + 3, message: `aaaa: ${i}` })
+interface Log {
+  id: string | number
+  level: 'Info' | 'Error' | 'Debug' | 'Warn'
+  message: string
 }
+
+const props = defineProps({
+  logs: {
+    type: Array<Log>,
+    required: true
+  }
+})
+
+const logViewer = ref<HTMLElement | null>(null)
+const atBottom = ref(true)
+
+const scrollToBottom = () => {
+  if (logViewer.value && atBottom.value) {
+    logViewer.value.scrollTop = logViewer.value.scrollHeight
+  }
+}
+const handleVisibleChange = (startIndex, endIndex, visibleItems) => {
+  if (logViewer.value) {
+    const { scrollTop, scrollHeight, clientHeight } = logViewer.value
+    atBottom.value = scrollTop + clientHeight >= scrollHeight - 1
+    console.log(atBottom.value)
+  }
+}
+
+const handleResize = () => {
+  scrollToBottom()
+}
+
+watch(() => props.logs.length, scrollToBottom)
+
+onMounted(() => {
+  scrollToBottom()
+})
 </script>
 
 <template>
-    <RecycleScroller class="w-full h-full p-4 bg-black text-white" :items="items" :item-size="28" key-field="id">
-      <template #default="{ item, index }">
-        <div class="font-bold">
-            {{ index }}: {{ item.message }}
-        </div>
-      </template>
+  <div ref="logViewer" class="w-full h-full p-4 bg-black text-white overflow-y-auto">
+    <RecycleScroller
+      class=""
+      :buffer="20"
+      :items="logs"
+      :item-size="24"
+      key-field="id"
+      @resize="handleResize"
+      @update="handleVisibleChange"
+      v-slot="{ item, index }"
+    >
+      <div class="text-sm">{{ index }}: {{ item.message }}</div>
     </RecycleScroller>
+  </div>
 </template>
 <style lang="scss" scoped></style>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,26 +1,32 @@
 <script lang="ts" setup>
-import { ref, watchEffect } from 'vue'
+import { ref, shallowReactive } from 'vue'
 import LogView from '@/components/LogView.vue';
 import useTimer from '@/hooks/useTimer';
-
-const test = ref(null)
 
 function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-const {result, isFirstLoading} = useTimer(async ()=> {
-  return new Date()
-}, {root: test})
 
-watchEffect(()=>{
-  console.log(result.value, isFirstLoading.value);  
+const logs = ref([])
+
+let i = 1
+
+for (let j = 0; j < 100; j ++) {
+  logs.value.push({ id: i, level: 'Info', message: `aaaa: ${new Date()}` })  
+  i ++
+}
+
+const {result, isFirstLoading} = useTimer(async ()=> {
+  await sleep(1000)
+  logs.value.push({ id: i, level: 'Info', message: `aaaa: ${new Date()}` })  
+  i ++
+  logs.value = [...logs.value]
 })
 
 </script>
 
 <template>
-  <div class="w-full h-[300px]">
-    <h1 v-for="i in 100" :key="i">{{ i }}</h1>
-    <div v-loading="isFirstLoading" ref="test"><LogView /></div>
+  <div class="w-[800px] h-[300px]">
+    <LogView :logs="logs" />
   </div>
 </template>
