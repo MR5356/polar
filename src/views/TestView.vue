13c8a4ab941086d6deaf93d3434a7c0bc3291189
Rui <marui1@ruijie.com.cn>
2024-07-26 17:02:33 +0800

    add networkCard

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -8,9 +8,10 @@ import MEMCard from '@/views/host/components/MEMCard.vue'
 import ColoredProgressBar from '@/components/ColoredProgressBar.vue'
 import DiskCard from '@/views/host/components/DiskCard.vue'
 import YearlyCountdown from '@/components/YearlyCountdown.vue'
+import NetworkCard from './host/components/NetworkCard.vue'
 
 function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 const logs = ref([])
@@ -62,70 +63,105 @@ const data = ref({
 
 const diskData = ref([
   {
-    "partition": "/dev/mmcblk0p1",
-    "size_bytes": 534763520,
-    "used_size_bytes": 67063808,
-    "mount_point": "/boot/firmware",
-    "fs_type": "vfat",
-    "read_speed_Bps": 0,
-    "write_speed_Bps": 0,
-    "read_iops": 0,
-    "write_iops": 0,
-    "read_latency_ms": 0,
-    "write_latency_ms": 0,
-    "total_read_bytes": 9305600,
-    "total_write_bytes": 1536
-  }
-  ,
+    partition: '/dev/mmcblk0p1',
+    size_bytes: 534763520,
+    used_size_bytes: 67063808,
+    mount_point: '/boot/firmware',
+    fs_type: 'vfat',
+    read_speed_Bps: 0,
+    write_speed_Bps: 0,
+    read_iops: 0,
+    write_iops: 0,
+    read_latency_ms: 0,
+    write_latency_ms: 0,
+    total_read_bytes: 9305600,
+    total_write_bytes: 1536
+  },
   {
-    "partition": "/dev/mmcblk0p2",
-    "size_bytes": 125348007936,
-    "used_size_bytes": 7057309696,
-    "mount_point": "/",
-    "fs_type": "ext4",
-    "read_speed_Bps": 0,
-    "write_speed_Bps": 299008,
-    "read_iops": 0,
-    "write_iops": 30,
-    "read_latency_ms": 0,
-    "write_latency_ms": 0,
-    "total_read_bytes": 1635037184,
-    "total_write_bytes": 231020683264
+    partition: '/dev/mmcblk0p2',
+    size_bytes: 125348007936,
+    used_size_bytes: 7057309696,
+    mount_point: '/',
+    fs_type: 'ext4',
+    read_speed_Bps: 0,
+    write_speed_Bps: 299008,
+    read_iops: 0,
+    write_iops: 30,
+    read_latency_ms: 0,
+    write_latency_ms: 0,
+    total_read_bytes: 1635037184,
+    total_write_bytes: 231020683264
+  }
+])
+
+const networkData = ref([
+  {
+    name: 'eth0',
+    type: 'ethernet',
+    bytes_rec: 299008,
+    bytes_sent: 1635037184,
+    speed_rec: 231020683264,
+    speed_sent: 163037184
+  },
+  {
+    name: 'wlp3s0',
+    type: 'wifi',
+    bytes_rec: 231020683264,
+    bytes_sent: 163500037184,
+    speed_rec: 231020683264,
+    speed_sent: 163037184
   }
 ])
 
 const { result, isFirstLoading } = useTimer(async () => {
+  networkData.value = [
+    {
+      name: 'eth0',
+      type: 'ethernet',
+      bytes_rec: Math.random() * 20000555508,
+      bytes_sent: Math.random() * 163500037184,
+      speed_rec: Math.random() * 20683264,
+      speed_sent: Math.random() * 163037184
+    },
+    {
+      name: 'wlp3s0',
+      type: 'wifi',
+      bytes_rec: Math.random() * 29000009008,
+      bytes_sent: Math.random() * 163005037184,
+      speed_rec: Math.random() * 23103264,
+      speed_sent: Math.random() * 163037184
+    }
+  ]
   diskData.value = [
     {
-      "partition": "/dev/mmcblk0p1",
-      "size_bytes": 534763520,
-      "used_size_bytes": Math.random() *534763520,
-      "mount_point": "/boot/firmware",
-      "fs_type": "vfat",
-      "read_speed_Bps": 0,
-      "write_speed_Bps": 0,
-      "read_iops": 0,
-      "write_iops": 0,
-      "read_latency_ms": 0,
-      "write_latency_ms": 0,
-      "total_read_bytes": Math.random() *9305600,
-      "total_write_bytes": Math.random() *1536
-    }
-    ,
+      partition: '/dev/mmcblk0p1',
+      size_bytes: 534763520,
+      used_size_bytes: Math.random() * 534763520,
+      mount_point: '/boot/firmware',
+      fs_type: 'vfat',
+      read_speed_Bps: 0,
+      write_speed_Bps: 0,
+      read_iops: 0,
+      write_iops: 0,
+      read_latency_ms: 0,
+      write_latency_ms: 0,
+      total_read_bytes: Math.random() * 9305600,
+      total_write_bytes: Math.random() * 1536
+    },
     {
-      "partition": "/dev/mmcblk0p2",
-      "size_bytes": 125348007936,
-      "used_size_bytes": Math.random() *125348007936,
-      "mount_point": "/",
-      "fs_type": "ext4",
-      "read_speed_Bps": 0,
-      "write_speed_Bps": Math.random() *299008,
-      "read_iops": 0,
-      "write_iops": 30,
-      "read_latency_ms": 0,
-      "write_latency_ms": 0,
-      "total_read_bytes": Math.random() *1635037184,
-      "total_write_bytes": Math.random() *231020683264
+      partition: '/dev/mmcblk0p2',
+      size_bytes: 125348007936,
+      used_size_bytes: Math.random() * 125348007936,
+      mount_point: '/',
+      fs_type: 'ext4',
+      read_speed_Bps: 0,
+      write_speed_Bps: Math.random() * 299008,
+      read_iops: 0,
+      write_iops: 30,
+      read_latency_ms: 0,
+      write_latency_ms: 0,
+      total_read_bytes: Math.random() * 1635037184,
+      total_write_bytes: Math.random() * 231020683264
     }
   ]
   memData.value = {
@@ -184,7 +220,6 @@ const { result, isFirstLoading } = useTimer(async () => {
   // i++
   // logs.value = [...logs.value]
 })
-
 </script>
 
 <template>
@@ -192,6 +227,7 @@ const { result, isFirstLoading } = useTimer(async () => {
     <!-- <YearlyCountdown /> -->
     <CPUCard :data="data" />
     <MEMCard :data="memData" />
+    <NetworkCard :data="networkData" />
     <DiskCard :data="diskData" />
   </div>
   <!--  <Terminal class="w-full h-full" uri="/api/v1/host/container/271bb15e-cae7-4a5d-a28b-2da7a75ac3bf/docker/container/bc1ae10c8eb603f9205d220ebfe5657a3b89eb3291b7594220b023746815efea/terminal?user=root"  />-->
---
+++ src/views/host/components/NetworkCard.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import numeral from 'numeral'
+import { Wifi, UpTwo, DownTwo } from '@icon-park/vue-next'
+
+interface NetworkInfo {
+  name: string
+  type: string
+  bytes_rec: number
+  bytes_sent: number
+  speed_rec: number
+  speed_sent: number
+}
+
+defineProps({
+  data: {
+    type: Array<NetworkInfo>,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="bg-white rounded-lg flex flex-col gap-2 min-w-[420px] text-sm select-none">
+    <template v-for="(item, index) in data" :key="index">
+      <div class="flex flex-col p-4 gap-2">
+        <div class="text-xs font-medium flex items-center gap-2">
+          <wifi theme="outline" size="16" fill="rgb(100 116 139)" />
+          <div>{{ item.name }}</div>
+        </div>
+        <div class="grid grid-cols-3">
+          <div class="flex flex-col items-start gap-1">
+            <div class="text-slate-500">{{ $t('component.networkCard.upload') }}</div>
+            <div class="text-xs font-medium">{{ numeral(item.bytes_sent).format('0b') }}</div>
+          </div>
+          <div class="flex flex-col items-center gap-1">
+            <div class="text-slate-500">{{ $t('component.networkCard.download') }}</div>
+            <div class="text-xs font-medium">{{ numeral(item.bytes_rec).format('0b') }}</div>
+          </div>
+          <div class="flex flex-col items-end gap-1">
+            <div class="font-bold text-xs text-slate-500 flex gap-1 items-center">
+              <div>{{ numeral(item.speed_sent).format('0b') }}&nbsp;/&nbsp;s</div>
+              <up-two theme="filled" size="14" fill="rgb(252 211 77)" />
+            </div>
+            <div class="font-bold text-xs text-slate-500 flex gap-1 items-center">
+              <div>{{ numeral(item.speed_rec).format('0b') }}&nbsp;/&nbsp;s</div>
+              <down-two theme="filled" size="14" fill="rgb(134 239 172)" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mx-4" :class="index !== data.length - 1 ? 'border-b-[1px]' : ''"></div>
+    </template>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
